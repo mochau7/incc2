8e0d6a181a700c55a578d143388949a20df17d6b
mochau7 <102217226+mochau7@users.noreply.github.com>
2022-12-02 23:18:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const nav1 = document.getElementById("link1");
+const nav2 = document.getElementById("link2");
+const nav3 = document.getElementById("link3");
+const nav4 = document.getElementById("link4");
+const nav5 = document.getElementById("link5");
+
+nav1.onmouseover = function () {
+  nav1.innerHTML =
+    "Valorant";
+};
+nav1.onmouseout = function () {
+  nav1.innerHTML =
+    "Home";
+};
+nav2.onmouseover = function () {
+  nav2.innerHTML =
+    "Wat is dat?";
+};
+nav2.onmouseout = function () {
+  nav2.innerHTML =
+    "Wat is valorant";
+};
+nav3.onmouseover = function () {
+  nav3.innerHTML =
+    "Riot Games";
+};
+nav3.onmouseout = function () {
+  nav3.innerHTML =
+    "Makers";
+};
+nav4.onmouseover = function () {
+  nav4.innerHTML =
+    "Locatie HQ";
+};
+nav4.onmouseout = function () {
+  nav4.innerHTML =
+    "Locatie";
+};
+
+nav5.onmouseover = function () {
+  nav5.innerHTML =
+    "E-mail";
+};
+nav5.onmouseout = function () {
+  nav5.innerHTML =
+    "contact";
+};
